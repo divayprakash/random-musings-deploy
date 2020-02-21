e8afa8ba8aafa2a50efe16e83d7661e56f1e89ed
Divay Prakash <divayprakash0@gmail.com>
2020-02-21 14:43:50 +0530

    Deploy 1223db9 : "Fix links for Netlify"

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -2,45 +2,45 @@ var store = [{
         "title": "Hello World!..... (maybe!)",
         "excerpt":"So….. after 3 days of getting to grips with Jekyll, wandering senselessly through Liquid and Textile and wading through the murky waters of Ruby, I’m finally here! The blog is complete as a local repository on my machine and now just the final push to GitHub Pages remains. Let’s see...","categories": ["init"],
         "tags": ["hello world","sweat"],
-        "url": "https://randmusings.github.io/init/2020/02/06/hello-world-maybe.html"
+        "url": "https://randommusings.netlify.com/init/2020/02/06/hello-world-maybe.html"
       },{
         "title": "Hello World!",
         "excerpt":"Finally…..we are live! :metal: And I can safely say ‘Hello World!’  :tada: :confetti_ball: :beers: :bowtie: ","categories": ["init"],
         "tags": ["hello world","made it!"],
-        "url": "https://randmusings.github.io/init/2020/02/06/hello-world.html"
+        "url": "https://randommusings.netlify.com/init/2020/02/06/hello-world.html"
       },{
         "title": "Flying High!",
         "excerpt":"So, my first post. Here goes……. I am writing this while 30,000 feet in the air on board a Boeing 737-85R, travelling from Mumbai to Guwahati. I had to being that up because it’d give me an opportunity to talk about my love of flying. I never thought of pursuing...","categories": ["flight"],
         "tags": ["flying","clouds"],
-        "url": "https://randmusings.github.io/flight/2020/02/08/flying-high.html"
+        "url": "https://randommusings.netlify.com/flight/2020/02/08/flying-high.html"
       },{
         "title": "Flying (not so) high",
         "excerpt":"As I said before, I type this now on my connecting flight, now on a Bombardier Q400NG Bash 8 bound for Dibrugarh. This really is one aircraft I love flying on. Somehow (and I really don’t actually know how) it turns out that these are far more comfortable as compared...","categories": ["flight"],
         "tags": ["flying"],
-        "url": "https://randmusings.github.io/flight/2020/02/08/flying-not-so-high.html"
+        "url": "https://randommusings.netlify.com/flight/2020/02/08/flying-not-so-high.html"
       },{
         "title": "Blogging like a hacker",
         "excerpt":"Let me just start off by saying that this post is (heavily) inspired by (or, creatively plagiarized from) Tom Preston-Werner’s original from 2008. So, with the disclaimer out of the way, let’s begin. I’ve been toying with the idea of setting up a blog for over 5 years now, almost...","categories": ["introspect"],
         "tags": ["blogging","exploration"],
-        "url": "https://randmusings.github.io/introspect/2020/02/16/blogging-like-a-hacker.html"
+        "url": "https://randommusings.netlify.com/introspect/2020/02/16/blogging-like-a-hacker.html"
       },{
         "title": "Misery",
         "excerpt":"At the point I last posted, I was just getting into exploring a new Jekyll theme for my blog. Now, after having sunk just over 3 days into the endeavour I find myself here, in abject misery. An overview of the past few hours - It started simply enough, implementing...","categories": ["blogging"],
         "tags": ["rant","frustration"],
-        "url": "https://randmusings.github.io/blogging/2020/02/19/misery.html"
+        "url": "https://randommusings.netlify.com/blogging/2020/02/19/misery.html"
       },{
         "title": "Ray of Hope",
         "excerpt":"'Rey' of hope!... No?... Ok, I'll show myself out...Finally, a possibility of light at the end of the tunnel. Considering the pit of despair I am in at the moment, a shred of hope is better than nothing at all. Browsing through some random forum1 gave me a possible solution...","categories": ["blogging"],
         "tags": ["hope","solution"],
-        "url": "https://randmusings.github.io/blogging/2020/02/19/ray-of-hope.html"
+        "url": "https://randommusings.netlify.com/blogging/2020/02/19/ray-of-hope.html"
       },{
         "title": "Success*",
         "excerpt":"When life gives you lemons, chunk it right back. Bill Waterson I guess life is handing out lemons today. Oh how I’d like to chunk them right back. Netlify works! It just works. It’s simple to set up and painless to maintain. All I had to do was revert to...","categories": ["blogging"],
         "tags": ["life","lemons"],
-        "url": "https://randmusings.github.io/blogging/2020/02/19/success.html"
+        "url": "https://randommusings.netlify.com/blogging/2020/02/19/success.html"
       },{
         "title": "It's done!",
         "excerpt":"After various false starts over the past years and days of trial and error (and the associated misery and joy) in this instance, I’m finally at a place where I can say that - The blog is done! At the very least, it’s done enough that I don’t feel the...","categories": ["blogging"],
         "tags": ["misadventure","success"],
-        "url": "https://randmusings.github.io/blogging/2020/02/21/it-s-done.html"
+        "url": "https://randommusings.netlify.com/blogging/2020/02/21/it-s-done.html"
       }]
