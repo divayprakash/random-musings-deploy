1edfbbe3f43ffb9d983e6a0101d0e7a2dcae7394
Divay Prakash <divayprakash0@gmail.com>
2020-02-29 13:05:50 +0530

    Deploy 02340e6 : "Deploy 27f9109 : "New post - 2020-02-26-so-much-to-do-and-so-much-time-to-do-it-in.md""

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -1,51 +1,51 @@
 var store = [{
         "title": "Hello World!..... (maybe!)",
-        "excerpt":"So….. after 3 days of getting to grips with Jekyll, wandering senselessly through Liquid and Textile and wading through the murky waters of Ruby, I’m finally here! The blog is complete as a local repository on my machine and now just the final push to GitHub Pages remains. Let’s see...","categories": ["init"],
-        "tags": ["hello world","sweat"],
-        "url": "https://randommusings.netlify.com/init/2020/02/06/hello-world-maybe.html"
+        "excerpt":"So….. after 3 days of getting to grips with Jekyll, wandering senselessly through Liquid and Textile and wading through the murky waters of Ruby, I’m finally here! The blog is complete as a local repository on my machine and now just the final push to GitHub Pages remains. Let’s see...","categories": ["blogging"],
+        "tags": ["hello world"],
+        "url": "http://localhost:4000/blogging/2020/02/06/hello-world-maybe.html"
       },{
         "title": "Hello World!",
-        "excerpt":"Finally…..we are live! :metal: And I can safely say ‘Hello World!’  :tada: :confetti_ball: :beers: :bowtie: ","categories": ["init"],
-        "tags": ["hello world","made it!"],
-        "url": "https://randommusings.netlify.com/init/2020/02/06/hello-world.html"
+        "excerpt":"Finally…..we are live! :metal: And I can safely say ‘Hello World!’  :tada: :confetti_ball: :beers: :bowtie: ","categories": ["blogging"],
+        "tags": ["hello world"],
+        "url": "http://localhost:4000/blogging/2020/02/06/hello-world.html"
       },{
         "title": "Flying High!",
-        "excerpt":"So, my first post. Here goes……. I am writing this while 30,000 feet in the air on board a Boeing 737-85R, travelling from Mumbai to Guwahati. I had to being that up because it’d give me an opportunity to talk about my love of flying. I never thought of pursuing...","categories": ["flight"],
-        "tags": ["flying","clouds"],
-        "url": "https://randommusings.netlify.com/flight/2020/02/08/flying-high.html"
+        "excerpt":"So, my first post. Here goes……. I am writing this while 30,000 feet in the air on board a Boeing 737-85R, travelling from Mumbai to Guwahati. I had to being that up because it’d give me an opportunity to talk about my love of flying. I never thought of pursuing...","categories": ["flying"],
+        "tags": ["clouds"],
+        "url": "http://localhost:4000/flying/2020/02/08/flying-high.html"
       },{
         "title": "Flying (not so) high",
-        "excerpt":"As I said before, I type this now on my connecting flight, now on a Bombardier Q400NG Bash 8 bound for Dibrugarh. This really is one aircraft I love flying on. Somehow (and I really don’t actually know how) it turns out that these are far more comfortable as compared...","categories": ["flight"],
-        "tags": ["flying"],
-        "url": "https://randommusings.netlify.com/flight/2020/02/08/flying-not-so-high.html"
+        "excerpt":"As I said before, I type this now on my connecting flight, now on a Bombardier Q400NG Bash 8 bound for Dibrugarh. This really is one aircraft I love flying on. Somehow (and I really don’t actually know how) it turns out that these are far more comfortable as compared...","categories": ["flying"],
+        "tags": ["little things"],
+        "url": "http://localhost:4000/flying/2020/02/08/flying-not-so-high.html"
       },{
         "title": "Blogging like a hacker",
-        "excerpt":"Let me just start off by saying that this post is (heavily) inspired by (or, creatively plagiarized from) Tom Preston-Werner’s original from 2008. So, with the disclaimer out of the way, let’s begin. I’ve been toying with the idea of setting up a blog for over 5 years now, almost...","categories": ["introspect"],
-        "tags": ["blogging","exploration"],
-        "url": "https://randommusings.netlify.com/introspect/2020/02/16/blogging-like-a-hacker.html"
+        "excerpt":"Let me just start off by saying that this post is (heavily) inspired by (or, creatively plagiarized from) Tom Preston-Werner’s original from 2008. So, with the disclaimer out of the way, let’s begin. I’ve been toying with the idea of setting up a blog for over 5 years now, almost...","categories": ["blogging","code"],
+        "tags": ["exploration"],
+        "url": "http://localhost:4000/blogging/code/2020/02/16/blogging-like-a-hacker.html"
       },{
         "title": "Misery",
-        "excerpt":"At the point I last posted, I was just getting into exploring a new Jekyll theme for my blog. Now, after having sunk just over 3 days into the endeavour I find myself here, in abject misery. An overview of the past few hours - It started simply enough, implementing...","categories": ["blogging"],
+        "excerpt":"At the point I last posted, I was just getting into exploring a new Jekyll theme for my blog. Now, after having sunk just over 3 days into the endeavour I find myself here, in abject misery. An overview of the past few hours - It started simply enough, implementing...","categories": ["blogging","code"],
         "tags": ["rant","frustration"],
-        "url": "https://randommusings.netlify.com/blogging/2020/02/19/misery.html"
+        "url": "http://localhost:4000/blogging/code/2020/02/19/misery.html"
       },{
         "title": "Ray of Hope",
-        "excerpt":"'Rey' of hope!... No?... Ok, I'll show myself out...Finally, a possibility of light at the end of the tunnel. Considering the pit of despair I am in at the moment, a shred of hope is better than nothing at all. Browsing through some random forum1 gave me a possible solution...","categories": ["blogging"],
+        "excerpt":"'Rey' of hope!... No?... Ok, I'll show myself out...Finally, a possibility of light at the end of the tunnel. Considering the pit of despair I am in at the moment, a shred of hope is better than nothing at all. Browsing through some random forum1 gave me a possible solution...","categories": ["blogging","code"],
         "tags": ["hope","solution"],
-        "url": "https://randommusings.netlify.com/blogging/2020/02/19/ray-of-hope.html"
+        "url": "http://localhost:4000/blogging/code/2020/02/19/ray-of-hope.html"
       },{
         "title": "Success*",
-        "excerpt":"When life gives you lemons, chunk it right back. Bill Waterson I guess life is handing out lemons today. Oh how I’d like to chunk them right back. Netlify works! It just works. It’s simple to set up and painless to maintain. All I had to do was revert to...","categories": ["blogging"],
+        "excerpt":"When life gives you lemons, chunk it right back. Bill Waterson I guess life is handing out lemons today. Oh how I’d like to chunk them right back. Netlify works! It just works. It’s simple to set up and painless to maintain. All I had to do was revert to...","categories": ["blogging","code"],
         "tags": ["life","lemons"],
-        "url": "https://randommusings.netlify.com/blogging/2020/02/19/success.html"
+        "url": "http://localhost:4000/blogging/code/2020/02/19/success.html"
       },{
         "title": "It's done!",
-        "excerpt":"After various false starts over the past years and days of trial and error (and the associated misery and joy) in this instance, I’m finally at a place where I can say that - The blog is done! At the very least, it’s done enough that I don’t feel the...","categories": ["blogging"],
+        "excerpt":"After various false starts over the past years and days of trial and error (and the associated misery and joy) in this instance, I’m finally at a place where I can say that - The blog is done! At the very least, it’s done enough that I don’t feel the...","categories": ["blogging","code"],
         "tags": ["misadventure","success"],
-        "url": "https://randommusings.netlify.com/blogging/2020/02/21/it-s-done.html"
+        "url": "http://localhost:4000/blogging/code/2020/02/21/it-s-done.html"
       },{
         "title": "So much to do... and SO MUCH time to do it in",
-        "excerpt":"In the words of the hallowed ‘Satchmo’ - There’s so little time and so much to do Louis “Satchmo” Armstrong At the moment, I am in my last semester of MBA studies at SCMHRD. Classes are over, and it’s only a few sporadic tests or assignment submissions here and there...","categories": ["blogging"],
+        "excerpt":"In the words of the hallowed ‘Satchmo’ - There’s so little time and so much to do Louis “Satchmo” Armstrong At the moment, I am in my last semester of MBA studies at SCMHRD. Classes are over, and it’s only a few sporadic tests or assignment submissions here and there...","categories": ["life"],
         "tags": ["boredom","time"],
-        "url": "https://randommusings.netlify.com/blogging/2020/02/26/so-much-to-do-and-so-much-time-to-do-it-in.html"
+        "url": "http://localhost:4000/life/2020/02/26/so-much-to-do-and-so-much-time-to-do-it-in.html"
       }]
